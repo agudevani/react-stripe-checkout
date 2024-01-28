c1d11e0dcf8a2ac45db1cadee23adfbd87803d37
agudevani <agusdevani1@gmail.com>
2024-01-28 12:08:47 -0300

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require ('express')
+const stripe = requiere ('stripe')
+const cors = require ('cors')
+
+const app = express ()
+
+apap.listen(30000, () => {
+    console.log('server on port', 3000)
+})
